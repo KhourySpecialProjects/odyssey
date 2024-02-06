40e409e149e99af86e9a3e7ee0f7790ffabaa068
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-02-06 16:04:51 -0500

    Support Digital Ocean Spaces

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: env("DO_CDN_URL"),
+        rootPath: env("DO_CDN_ROOT_PATH"),
+        s3Options: {
+          accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+          secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+          region: env("DO_SPACE_REGION"),
+          endpoint: env("DO_SPACE_ENDPOINT"),
+          params: {
+            Bucket: env("DO_SPACE_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
