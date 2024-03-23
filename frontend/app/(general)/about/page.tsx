4e0e80a30082aea994e03b883cc45d7a3d40c618
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-03-23 18:36:15 -0400

    Abstract gradient background element

--- frontend/app/(general)/about/page.tsx
+++ frontend/app/(general)/about/page.tsx
@@ -1,3 +1,4 @@
+import { GradientBackground } from "@/components/gradient-bg";
 import { Button } from "@/components/ui/button";
 import {
   ArrowRightIcon,
@@ -52,131 +53,119 @@ const focusAreas = [
 
 export default async function AboutPage() {
   return (
-    <div className="px-6 py-12 bg-white isolate sm:py-16 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
-      >
-        {/* purple version: from-[#ff80b5] to-[#9089fc] */}
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#FFAF80] to-[#A0E0EF] opacity-60 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
+    <GradientBackground>
+      <>
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+            About Odyssey
+          </h2>
+          <p className="mt-4 text-lg leading-normal text-slate-600 text-balance">
+            Odyssey is an all-new on-demand learning platform built by Khoury
+            College, for Khoury College.
+          </p>
+        </div>
 
-      <div className="max-w-2xl mx-auto text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-          About Odyssey
-        </h2>
-        <p className="mt-4 text-lg leading-normal text-slate-600 text-balance">
-          Odyssey is an all-new on-demand learning platform built by Khoury
-          College, for Khoury College.
-        </p>
-      </div>
-
-      <div className="px-6 mx-auto mt-24 max-w-7xl lg:px-8">
-        <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8 lg:pt-4">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-sky-600">
-                Types
-              </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-                Understand{" "}
-                <span className="italic underline underline-offset-1 font-mono px-1.5 text-sky-600">
-                  how
-                </span>{" "}
-                you&rsquo;ll learn
-              </p>
-              <p className="mt-6 text-lg leading-8 text-slate-600">
-                Droplets are categorized based on the types of content they
-                include and their estimated completion time, so that you know
-                what to expect and how to make the most of your odyssey.
-              </p>
-            </div>
-          </div>
-
-          <dl className="max-w-xl space-y-4 text-base leading-7 text-slate-600 lg:max-w-none">
-            {types.map((type) => (
-              <div
-                key={type.name.toLowerCase()}
-                className="p-5 transition-colors border border-transparent rounded-md bg-sky-50 hover:border-sky-600"
-              >
-                <dt className="flex flex-row items-center gap-3">
-                  <type.icon
-                    className="w-6 h-6 text-sky-600"
-                    aria-hidden="true"
-                  />
-                  <span className="text-lg font-semibold text-slate-900">
-                    {type.name} Droplets
-                  </span>
-                </dt>
-                <dd className="mt-1 ml-9">{type.description}</dd>
+        <div className="px-6 mx-auto mt-24 max-w-7xl lg:px-8">
+          <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div className="lg:pr-8 lg:pt-4">
+              <div className="lg:max-w-lg">
+                <h2 className="text-base font-semibold leading-7 text-sky-600">
+                  Types
+                </h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+                  Understand{" "}
+                  <span className="italic underline underline-offset-1 font-mono px-1.5 text-sky-600">
+                    how
+                  </span>{" "}
+                  you&rsquo;ll learn
+                </p>
+                <p className="mt-6 text-lg leading-8 text-slate-600">
+                  Droplets are categorized based on the types of content they
+                  include and their estimated completion time, so that you know
+                  what to expect and how to make the most of your odyssey.
+                </p>
               </div>
-            ))}
-          </dl>
-        </div>
-      </div>
-
-      <div className="px-6 mx-auto mt-24 max-w-7xl lg:px-8">
-        <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8 lg:pt-4">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-sky-600">
-                Focus Areas
-              </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-                Understand{" "}
-                <span className="italic underline underline-offset-1 font-mono px-1.5 text-sky-600">
-                  what
-                </span>{" "}
-                you&rsquo;ll learn
-              </p>
-              <p className="mt-6 text-lg leading-8 text-slate-600">
-                Odyssey has collections of Droplets across a number of focus
-                areas to hone in on various student aspects.
-              </p>
             </div>
+
+            <dl className="max-w-xl space-y-4 text-base leading-7 text-slate-600 lg:max-w-none">
+              {types.map((type) => (
+                <div
+                  key={type.name.toLowerCase()}
+                  className="p-5 transition-colors border border-transparent rounded-md bg-sky-50 hover:border-sky-600"
+                >
+                  <dt className="flex flex-row items-center gap-3">
+                    <type.icon
+                      className="w-6 h-6 text-sky-600"
+                      aria-hidden="true"
+                    />
+                    <span className="text-lg font-semibold text-slate-900">
+                      {type.name} Droplets
+                    </span>
+                  </dt>
+                  <dd className="mt-1 ml-9">{type.description}</dd>
+                </div>
+              ))}
+            </dl>
           </div>
+        </div>
 
-          <dl className="max-w-xl space-y-4 text-base leading-7 text-slate-600 lg:max-w-none">
-            {focusAreas.map((type) => (
-              <div
-                key={type.name.toLowerCase()}
-                className="p-5 transition-colors border border-transparent rounded-md bg-sky-50 hover:border-sky-600"
-              >
-                <dt className="flex flex-row items-center gap-3">
-                  <type.icon
-                    className="w-6 h-6 text-sky-600"
-                    aria-hidden="true"
-                  />
-                  <span className="text-lg font-semibold text-slate-900">
-                    {type.name} Droplets
-                  </span>
-                </dt>
-                <dd className="mt-1 ml-9">{type.description}</dd>
+        <div className="px-6 mx-auto mt-24 max-w-7xl lg:px-8">
+          <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div className="lg:pr-8 lg:pt-4">
+              <div className="lg:max-w-lg">
+                <h2 className="text-base font-semibold leading-7 text-sky-600">
+                  Focus Areas
+                </h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+                  Understand{" "}
+                  <span className="italic underline underline-offset-1 font-mono px-1.5 text-sky-600">
+                    what
+                  </span>{" "}
+                  you&rsquo;ll learn
+                </p>
+                <p className="mt-6 text-lg leading-8 text-slate-600">
+                  Odyssey has collections of Droplets across a number of focus
+                  areas to hone in on various student aspects.
+                </p>
               </div>
-            ))}
-          </dl>
-        </div>
-      </div>
+            </div>
 
-      <div className="max-w-2xl mx-auto mt-16 text-center sm:mt-20">
-        <h2 className="text-xl font-bold tracking-tight text-slate-900 sm:text-2xl">
-          Ready to join the Odyssey?
-        </h2>
-        <div className="mt-6">
-          <Button size="lg" after={<ArrowRightIcon />} asChild>
-            <Link href="/request-access">Request Access</Link>
-          </Button>
-          <Button size="lg" variant="link" after={<ArrowRightIcon />} asChild>
-            <Link href="/explore">Explore Droplets</Link>
-          </Button>
+            <dl className="max-w-xl space-y-4 text-base leading-7 text-slate-600 lg:max-w-none">
+              {focusAreas.map((type) => (
+                <div
+                  key={type.name.toLowerCase()}
+                  className="p-5 transition-colors border border-transparent rounded-md bg-sky-50 hover:border-sky-600"
+                >
+                  <dt className="flex flex-row items-center gap-3">
+                    <type.icon
+                      className="w-6 h-6 text-sky-600"
+                      aria-hidden="true"
+                    />
+                    <span className="text-lg font-semibold text-slate-900">
+                      {type.name} Droplets
+                    </span>
+                  </dt>
+                  <dd className="mt-1 ml-9">{type.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
         </div>
-      </div>
-    </div>
+
+        <div className="max-w-2xl mx-auto mt-16 text-center sm:mt-20">
+          <h2 className="text-xl font-bold tracking-tight text-slate-900 sm:text-2xl">
+            Ready to join the Odyssey?
+          </h2>
+          <div className="mt-6">
+            <Button size="lg" after={<ArrowRightIcon />} asChild>
+              <Link href="/request-access">Request Access</Link>
+            </Button>
+            <Button size="lg" variant="link" after={<ArrowRightIcon />} asChild>
+              <Link href="/explore">Explore Droplets</Link>
+            </Button>
+          </div>
+        </div>
+      </>
+    </GradientBackground>
   );
 }
--- frontend/app/(general)/auth/login/page.tsx
+++ frontend/app/(general)/auth/login/page.tsx
@@ -1,3 +1,4 @@
+import { GradientBackground } from "@/components/gradient-bg";
 import { authOptions } from "@/lib/auth/options";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth/next";
@@ -22,33 +23,22 @@ export default async function SignIn() {
   const providers = await getProviders();
 
   return (
-    <div className="isolate bg-white px-6 py-12 sm:py-16 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
+    <GradientBackground>
+      <>
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+            Log In
+          </h2>
+          <p className="mt-4 text-lg leading-normal text-slate-600 text-balance">
+            Authenticate with your Northeastern University account via Azure
+            Active Directory to access Khoury Odyssey.
+          </p>
+        </div>
 
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-          Log In
-        </h2>
-        <p className="mt-4 text-lg leading-normal text-slate-600 text-balance">
-          Authenticate with your Northeastern University account via Azure
-          Active Directory to access Khoury Odyssey.
-        </p>
-      </div>
-
-      <div className="mx-auto mt-8 max-w-2xl sm:mt-12 text-center">
-        <LoginButtons providers={providers} />
-      </div>
-    </div>
+        <div className="max-w-2xl mx-auto mt-8 text-center sm:mt-12">
+          <LoginButtons providers={providers} />
+        </div>
+      </>
+    </GradientBackground>
   );
 }
