bd7c5285073dd02373b768d092aa7dcb36e91ae0
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-03-26 12:03:14 -0400

    Disable PostHog in non-prod environments

--- frontend/providers/PHProvider.tsx
+++ frontend/providers/PHProvider.tsx
@@ -3,6 +3,7 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
+
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
@@ -10,5 +11,10 @@ if (typeof window !== "undefined") {
 }
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
+  // disable analytics in non-prod environments
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
