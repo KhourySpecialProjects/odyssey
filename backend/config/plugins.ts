7c341efbc610005f602e9366cf4e521ea8459177
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-04-03 15:41:59 -0400

    Set up Swagger documentation

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,4 +1,41 @@
 export default ({ env }) => ({
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: "3.0.0",
+      info: {
+        version: "1.0.0",
+        title: "Khoury Odyssey CMS: API Docs",
+        description:
+          "API documentation for Khoury Odyssey's content management system.",
+        termsOfService: null,
+        contact: {
+          name: "Khoury Odyssey Team",
+          email: "khouryodyssey@northeastern.edu",
+          url: "https://khouryodyssey.org",
+        },
+        license: null,
+      },
+      "x-strapi-config": {
+        // Leave empty to ignore plugins during generation
+        // plugins: ["upload", "users-permissions"],
+        plugins: ["upload"],
+        path: "/documentation",
+      },
+      servers: [
+        { url: "http://localhost:1337/api", description: "Development server" },
+        {
+          url: "https://data.khouryodyssey.org/api",
+          description: "Production server",
+        },
+      ],
+      externalDocs: {
+        description: "Strapi Docs",
+        url: "https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html",
+      },
+      security: [{ bearerAuth: [] }],
+    },
+  },
   upload: {
     config: {
       provider: "aws-s3",
