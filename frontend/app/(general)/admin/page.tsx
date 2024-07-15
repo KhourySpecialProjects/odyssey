31b97ee008622909eec7f5ebf2854950c59b90a8
bpalazzi512 <60822022+bpalazzi512@users.noreply.github.com>
2024-07-15 09:21:46 -0400

    fixing prettier

--- frontend/app/(general)/admin/page.tsx
+++ frontend/app/(general)/admin/page.tsx
@@ -6,24 +6,20 @@ import { AuthorizedUsers } from "@/components/admin/users/authorized-users";
 import { getCurrentUser } from "@/lib/auth/session";
 import { notFound } from "next/navigation";
 
-
 export default async function Page() {
   const user = await getCurrentUser();
   if (!user || !user.isAdmin) return notFound();
 
   return (
-    
     <div className="w-full max-w-5xl p-8 mx-auto space-y-12">
-
-    
       <Session />
-        <AdminSelector content={{
-          'Authorized Users': <AuthorizedUsers />,
-          'Access Requests': <AccessRequests />,
-          'Reports': <Reports />,
-        }}/>
+      <AdminSelector
+        content={{
+          "Authorized Users": <AuthorizedUsers />,
+          "Access Requests": <AccessRequests />,
+          Reports: <Reports />,
+        }}
+      />
     </div>
-      
-    
   );
 }
