cf99ecd522231e2c90910c9b8d91c492cfae100a
bpalazzi512 <60822022+bpalazzi512@users.noreply.github.com>
2024-07-15 08:56:21 -0400

    initial commit

--- frontend/app/(general)/admin/page.tsx
+++ frontend/app/(general)/admin/page.tsx
@@ -1,20 +1,29 @@
 import { AccessRequests } from "@/components/admin/access-requests/access-requests";
 import { Reports } from "@/components/admin/reports/reports";
 import { Session } from "@/components/admin/session";
+import { AdminSelector } from "@/components/admin/selector";
 import { AuthorizedUsers } from "@/components/admin/users/authorized-users";
 import { getCurrentUser } from "@/lib/auth/session";
 import { notFound } from "next/navigation";
+
 
 export default async function Page() {
   const user = await getCurrentUser();
   if (!user || !user.isAdmin) return notFound();
 
   return (
+    
     <div className="w-full max-w-5xl p-8 mx-auto space-y-12">
+
+    
       <Session />
-      <AccessRequests />
-      <Reports />
-      <AuthorizedUsers />
+        <AdminSelector content={{
+          'Authorized Users': <AuthorizedUsers />,
+          'Access Requests': <AccessRequests />,
+          'Reports': <Reports />,
+        }}/>
     </div>
+      
+    
   );
 }
