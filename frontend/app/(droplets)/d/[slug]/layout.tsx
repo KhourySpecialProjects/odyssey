0db3b0381e820cbe1153c3aa3756933d1e09ec9a
Vasilis Gadala <vasilisgadala@gmail.com>
2024-09-30 21:34:36 -0700

    updated banner to appear within droplets and drafts. Lessons within droplets can be dragged to change order, automatically saving to persist changes

--- frontend/app/(droplets)/d/[slug]/layout.tsx
+++ frontend/app/(droplets)/d/[slug]/layout.tsx
@@ -47,7 +47,7 @@ export default async function RootLayout({ params, children }: Props) {
       <Sidebar user={user} droplet={droplet} />
 
       <div className="md:ml-64">
-        <DebugBanner className="mb-2" />
+        <DebugBanner />
         <EnvironmentBanner className="mb-2" />
 
         <div className="p-6 rounded-lg sm:p-8 md:py-10 md:m-4 md:border-dashed md:border-2 md:border-slate-200 md:dark:border-slate-700">
