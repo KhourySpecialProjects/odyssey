01be7ce121598f57979b2c25a5901b1613f2273b
martin0he <martin.hema.mh@gmail.com>
2024-11-17 14:58:32 -0500

    first draft of new create new droplet form

--- frontend/lib/actions.ts
+++ frontend/lib/actions.ts
@@ -29,7 +29,7 @@ const CreateAuthorizedUser = AuthorizedUserSchema.omit({
 });
 export async function createAuthorizedUser(prevState: any, formData: FormData) {
   const roleID = await getAuthorizedUserRoleIdByTitle(
-    AuthorizedUserRoleTitle.User,
+    AuthorizedUserRoleTitle.User
   );
   const { email, isEnabled } = CreateAuthorizedUser.parse({
     email: formData.get("email"),
@@ -90,7 +90,7 @@ export async function updateAuthorizedUser(formData: FormData) {
           "Content-Type": "application/json",
           Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
         },
-      },
+      }
     );
     const data = await response.json();
     if (!response.ok || (response.ok && data.error))
@@ -120,7 +120,7 @@ export async function deleteAuthorizedUser(formData: FormData) {
           "Content-Type": "application/json",
           Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
         },
-      },
+      }
     );
     const data = await response.json();
     if (!response.ok || (response.ok && data.error))
@@ -134,7 +134,7 @@ export async function deleteAuthorizedUser(formData: FormData) {
 }
 
 export async function createAccessRequest(
-  formData: z.infer<typeof accessRequestSchema>,
+  formData: z.infer<typeof accessRequestSchema>
 ) {
   try {
     const response = await fetch(STRAPI_API_URL + "/api/access-requests", {
@@ -175,7 +175,7 @@ export async function deleteAccessRequest(formData: FormData) {
           "Content-Type": "application/json",
           Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
         },
-      },
+      }
     );
     const data = await response.json();
     if (!response.ok || (response.ok && data.error))
@@ -246,7 +246,7 @@ export async function updateAuthorBio(formData: z.infer<typeof BioFormSchema>) {
 }
 
 export async function createEnrollment(
-  formData: z.infer<typeof DropletEnrollmentSchema>,
+  formData: z.infer<typeof DropletEnrollmentSchema>
 ) {
   try {
     const user = await getCurrentUser();
@@ -388,7 +388,7 @@ export async function updateDroplet(
   options: { regenerateSlug?: boolean; revalidate?: boolean } = {
     regenerateSlug: false,
     revalidate: false,
-  },
+  }
 ) {
   try {
     const dataToSend: any = {
@@ -454,7 +454,7 @@ export async function updateLesson(
   options: { reload?: boolean; regenerateSlug?: boolean } = {
     reload: false,
     regenerateSlug: false,
-  },
+  }
 ) {
   try {
     if (data.blocks) {
