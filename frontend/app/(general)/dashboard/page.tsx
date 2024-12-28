cb490fd5f3541d1c63176ef7896abef2b29ad6e7
Mark Fontenot <m.fontenot@northeastern.edu>
2024-12-28 04:56:25 -0500

    Prettier updates

--- frontend/app/(general)/dashboard/page.tsx
+++ frontend/app/(general)/dashboard/page.tsx
@@ -2,21 +2,11 @@ import { MyContent } from "@/components/dashboard/my-content";
 import { DropletsSkeleton } from "@/components/explore/droplets-skeleton";
 import { Suspense } from "react";
 
-// type Props = {
-//   params: Promise<Params>;
-// };
-
-// type Params = {
-//   slug: string;
-//   searchParams: { [key: string]: string | string[] | undefined };
-// };
-
 type Props = {
   searchParams?: { [key: string]: string | string[] | undefined };
 };
 
 export default async function DashboardRoute({ searchParams }: Props) {
-  // const searchParams = (await params).searchParams;
   return (
     <>
       <div className="w-full p-8 mx-auto my-4 text-center max-w-7xl">
