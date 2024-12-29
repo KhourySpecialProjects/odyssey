72416171d90b152495cd91a30f925c06b2226baf
Mark Fontenot <m.fontenot@northeastern.edu>
2024-12-29 10:29:18 -0500

    Updated the main application header to include My Groups option

--- frontend/config/general.ts
+++ frontend/config/general.ts
@@ -3,16 +3,20 @@ import { GeneralConfig } from "@/types";
 export const generalConfig: GeneralConfig = {
   mainNav: [
     {
-      href: "/about",
-      label: "About",
-    },
-    {
       href: "/explore",
       label: "Explore",
     },
     {
       href: "/dashboard",
       label: "My Content",
+    },
+    {
+      href: "/g/dashboard",
+      label: "My Groups",
+    },
+    {
+      href: "/about",
+      label: "About",
     },
   ],
 
@@ -28,6 +32,10 @@ export const generalConfig: GeneralConfig = {
     {
       href: "/dashboard",
       label: "My Content",
+    },
+    {
+      href: "/g/dashboard",
+      label: "My Groups",
     },
     {
       href: "/drafts",
