57e9711f12f54825f97ed96a10e2737aa1ff187b
Mark Fontenot <m.fontenot@northeastern.edu>
2025-01-04 10:14:52 -0500

    Modified lesson renderer quiz question to handle HTML from tiptap.

--- frontend/components/droplets/lessons/quiz-question.tsx
+++ frontend/components/droplets/lessons/quiz-question.tsx
@@ -30,7 +30,7 @@ export function QuizQuestionBlock({ question }: { question: QuizQuestion }) {
   const [showResult, setShowResult] = useState(false);
   const correctAnswer = useMemo(
     () => question.answerOptions.find((option) => option.isCorrect),
-    [question],
+    [question]
   );
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -69,8 +69,7 @@ export function QuizQuestionBlock({ question }: { question: QuizQuestion }) {
               <p className="mt-4 font-bold text-pretty">
                 {
                   question.answerOptions.find(
-                    (option) =>
-                      String(option.id) === form.getValues("answerId"),
+                    (option) => String(option.id) === form.getValues("answerId")
                   )!.content
                 }
               </p>
@@ -87,7 +86,7 @@ export function QuizQuestionBlock({ question }: { question: QuizQuestion }) {
                   {
                     question.answerOptions.find(
                       (option) =>
-                        String(option.id) === form.getValues("answerId"),
+                        String(option.id) === form.getValues("answerId")
                     )!.content
                   }
                 </p>
@@ -141,7 +140,12 @@ export function QuizQuestionBlock({ question }: { question: QuizQuestion }) {
                                       ? "D"
                                       : "?"}
                             </span>
-                            {answer.content}
+                            <div
+                              className="prose prose-m w-full"
+                              dangerouslySetInnerHTML={{
+                                __html: answer.content,
+                              }}
+                            />
                           </FormLabel>
                         </FormItem>
                       ))}
