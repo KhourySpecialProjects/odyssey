ff54c0ac4b2f6c164b4ea5699149bcceff9624fa
chouser63 <houser.ch@northeastern.edu>
2025-01-16 15:53:57 -0500

    fixed some bugs related to adding & editing users

--- frontend/components/admin/users/authorized-user.tsx
+++ frontend/components/admin/users/authorized-user.tsx
@@ -17,9 +17,9 @@ import { type } from "os";
 export function AuthorizedUserBlock({ user }: { user: AuthorizedUser }) {
   const [open, setOpen] = useState(false);
   const isAdmin = isAuthorizedUserAdmin(user.roles.map((role) => role.title));
-  const [firstName, setFirstName] = useState(user.firstName);
-  const [lastName, setLastName] = useState(user.lastName);
-  const [bio, setBio] = useState(user.bio);
+  const [firstName, setFirstName] = useState(user.firstName || "");
+  const [lastName, setLastName] = useState(user.lastName || "");
+  const [bio, setBio] = useState(user.bio || "");
   const [selectedRoles, setSelectedRoles] = useState<AuthorizedUserRoleTitle[]>(
     user.roles.map(role => role.title)
   );
