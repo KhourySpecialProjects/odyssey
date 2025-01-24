99307b04ccc8aeaae5777dfca936985acde76049
gpalmer27 <palmer.gi@northeastern.edu>
2025-01-24 12:02:12 -0500

    added friend request features

--- frontend/components/friends/friend-block.tsx
+++ frontend/components/friends/friend-block.tsx
@@ -1,44 +1,46 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { updateAuthorizedUser, updateUserInfo } from "@/lib/actions";
+import { removeFriend } from "@/lib/requests/friends";
 import { AuthorizedUser } from "@/types";
-import { Pencil } from "lucide-react";
-import { useFormStatus } from "react-dom";
-import { isAuthorizedUserAdmin } from "@/lib/utils";
-import { useState } from "react";
-import {
-  DialogHeader,
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogTitle,
-  DialogDescription,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
+import { startTransition } from "react";
 import { toast } from "sonner";
-import { AuthorizedUserRoleTitle } from "@/lib/globals";
-import { Checkbox } from "@/components/ui/checkbox";
-import { type } from "os";
 
-export function FriendBlock({ user }: { user: AuthorizedUser }) {
+
+
+export function FriendBlock({ user, friend }: { user: AuthorizedUser, friend: AuthorizedUser }) {
+
+  const handleRemove = () => {
+    startTransition(async () => {
+      const result = await removeFriend(user.id, friend.id);
+      if (result.success) {
+        toast.success("Friend removed");
+      } else {
+        toast.error("Failed to remove friend");
+      }
+    });
+  };
     return (
       <li className="py-0 [&:not(:first-child)]:pt-3">
         <div className="flex items-center space-x-4">
           <div className="flex-1 min-w-0">
             <p className="font-medium truncate text-slate-900 dark:text-white">
-              {user.firstName && user.lastName 
-                ? `${user.firstName} ${user.lastName}`
-                : user.email}
+              {friend.firstName && friend.lastName 
+                ? `${friend.firstName} ${friend.lastName}`
+                : friend.email}
             </p>
-            {user.bio && (
+            {friend.bio && (
               <p className="text-sm truncate text-slate-500 dark:text-slate-400">
-                {user.bio}
+                {friend.bio}
               </p>
             )}
           </div>
-  
           <div className="inline-flex items-center gap-2">
+            <Button  size="sm" variant="outline" >
+              View Profile
+            </Button>
+          </div>
+          <div className="inline-flex items-center gap-2" onClick={handleRemove}>
             <Button size="sm" variant="outline">
               Remove Friend
             </Button>
@@ -47,24 +49,3 @@ export function FriendBlock({ user }: { user: AuthorizedUser }) {
       </li>
     );
   }
-
-function SubmitButton({
-  destructive,
-  children,
-}: {
-  destructive?: boolean;
-  children: React.ReactNode;
-}) {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button
-      type="submit"
-      size="sm"
-      variant={destructive ? "destructive" : "link"}
-      aria-disabled={pending}
-    >
-      {children}
-    </Button>
-  );
-}
--- frontend/lib/requests/friends.ts
+++ frontend/lib/requests/friends.ts
@@ -1,17 +1,38 @@
+'use server'
 import { fetchAPI, flattenAttributes } from "@/lib/utils";
 import { AuthorizedUser } from "@/types";
 import { StrapiRequestParams } from "@/types/strapi";
+import { revalidatePath } from "next/cache";
 import qs from "qs";
 
 const NEXT_PUBLIC_STRAPI_API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL;
 const STRAPI_ACCESS_TOKEN = process.env.STRAPI_ACCESS_TOKEN;
 
-export async function fetchFriends(): Promise<AuthorizedUser[]> {
+interface Friendship {
+  friends: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    bio: string;
+  }[];
+}
+
+export async function fetchFriends(authorizedUser: AuthorizedUser): Promise<AuthorizedUser[]> {
+  console.log("user", authorizedUser)
+  console.log("id", authorizedUser.id)
   try {
     const query = qs.stringify({
+      filters: {
+        friends: {
+          id: {
+            $eq: authorizedUser.id
+          }
+        }
+      },
       populate: {
         friends: {
-          fields: ["id", "email", "firstName", "lastName", "bio"]
+          fields: ["id", "email", "firstName", "lastName", "bio", "received_requests"]
         }
       },
       pagination: {
@@ -19,6 +40,7 @@ export async function fetchFriends(): Promise<AuthorizedUser[]> {
         page: 1,
       },
     });
+
     const response = await fetch(
       NEXT_PUBLIC_STRAPI_API_URL + "/api/friendships?" + query,
       {
@@ -27,39 +49,164 @@ export async function fetchFriends(): Promise<AuthorizedUser[]> {
       },
     );
     const data = await response.json();
-    const authorizedUsers = flattenAttributes(data.data);
-    return authorizedUsers;
+    const friendships = flattenAttributes(data.data);
+    
+    return friendships
+      .flatMap((friendship: Friendship) => 
+        friendship.friends.filter(friend => friend.id !== authorizedUser.id)
+      );
   } catch (error) {
     console.error("Database Error:", error);
-    throw new Error("Failed to fetch authorized users data.");
+    throw new Error("Failed to fetch friends data.");
   }
 }
 
-export async function fetchFriendRequests(): Promise<AuthorizedUser[]> {
-    try {
-      const query = qs.stringify({
-        populate: {
-          friends: {
-            fields: ["id", "email", "firstName", "lastName", "bio"]
-          }
-        },
-        pagination: {
-          pageSize: 25,
-          page: 1,
-        },
-      });
-      const response = await fetch(
-        NEXT_PUBLIC_STRAPI_API_URL + "/api/friendships?" + query,
-        {
-          headers: { Authorization: "Bearer " + STRAPI_ACCESS_TOKEN },
-          cache: "no-store",
-        },
-      );
-      const data = await response.json();
-      const authorizedUsers = flattenAttributes(data.data);
-      return authorizedUsers;
-    } catch (error) {
-      console.error("Database Error:", error);
-      throw new Error("Failed to fetch authorized users data.");
+export async function acceptFriendRequest(userId: number, requestId: number) {
+  const token = process.env.STRAPI_ACCESS_TOKEN;
+  try {
+    const friendshipResponse = await fetch(
+      NEXT_PUBLIC_STRAPI_API_URL + "/api/friendships",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          data: {
+            friends: [requestId, userId],
+          },
+        }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
+        },    
+      }
+    );
+  
+    if (!friendshipResponse.ok) {
+      console.error("Friendship Response:", await friendshipResponse.text());
+      throw new Error("Failed to create friendship");
     }
+    const deleteResponse = await fetch(
+      `${NEXT_PUBLIC_STRAPI_API_URL}/api/authorized-users/${userId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
+        },
+        body: JSON.stringify({
+          data: {
+            received_requests: {
+              disconnect: [requestId]
+            }
+          },
+        }),
+      }
+    );
+  
+    if (!deleteResponse.ok) {
+      console.error("Delete Response:", await deleteResponse.text());
+      throw new Error("Failed to delete friend request");
+    }
+    revalidatePath('/settings/friends');
+  return { success: true };
+  } catch (error) {
+    console.error("Error:", error);
+    return { success: false, error };
   }
+}
+
+export async function rejectFriendRequest(userId: number, requestId: number) {
+  const token = process.env.STRAPI_ACCESS_TOKEN;
+  try {
+    const deleteResponse = await fetch(
+      `${NEXT_PUBLIC_STRAPI_API_URL}/api/authorized-users/${userId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
+        },
+        body: JSON.stringify({
+          data: {
+            received_requests: {
+              disconnect: [requestId]
+            }
+          },
+        }),
+      }
+    );
+  
+    if (!deleteResponse.ok) {
+      console.error("Delete Response:", await deleteResponse.text());
+      throw new Error("Failed to delete friend request");
+    }
+    revalidatePath('/settings/friends');
+  return { success: true };
+  } catch (error) {
+    console.error("Error:", error);
+    return { success: false, error };
+  }
+}
+
+export async function removeFriend(userId: number, friendId: number) {
+  const token = process.env.STRAPI_ACCESS_TOKEN;
+  try {
+    const query = qs.stringify({
+      filters: {
+        $and: [
+          {
+            friends: {
+              id: {
+                $eq: userId
+              }
+            }
+          },
+          {
+            friends: {
+              id: {
+                $eq: friendId
+              }
+            }
+          }
+        ]
+      }
+    });
+
+    const findResponse = await fetch(
+      `${NEXT_PUBLIC_STRAPI_API_URL}/api/friendships?${query}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const data = await findResponse.json();
+    const friendship = data.data[0];
+
+    if (!friendship) {
+      console.error("friendship not found")
+      throw new Error("Friendship not found");
+    }
+
+    const deleteResponse = await fetch(
+      `${NEXT_PUBLIC_STRAPI_API_URL}/api/friendships/${friendship.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + STRAPI_ACCESS_TOKEN,
+        },
+      }
+    );
+  
+    if (!deleteResponse.ok) {
+      console.error("Delete Response:", await deleteResponse.text());
+      throw new Error("Failed to delete friendship");
+    }
+    revalidatePath('/settings/friends');
+  return { success: true };
+  } catch (error) {
+    console.error("Error:", error);
+    return { success: false, error };
+  }
+}
