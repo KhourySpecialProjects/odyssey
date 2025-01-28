c446a795490cbbf7c245f54e0417bc4184e78976
chouser63 <houser.ch@northeastern.edu>
2025-01-28 16:50:44 -0500

    fixed bug with search bar

--- frontend/components/friends/friend-search.tsx
+++ frontend/components/friends/friend-search.tsx
@@ -11,77 +11,84 @@ import { AuthorizedUserBlock } from "../admin/users/authorized-user";
 import { FriendSuggestionsBlock } from "./friend-suggestions-block";
 
 interface FriendSearchProps {
-  authUsers: AuthorizedUser[];
-  curUser: AuthorizedUser;
+    authUsers: AuthorizedUser[];
+    curUser: AuthorizedUser;
 }
 
 export function FriendSearch({ authUsers, curUser }: FriendSearchProps) {
-  const [searchTerm, setSearchItem] = useState("");
-  const [isHovered, setIsHovered] = useState(false);
-  const [searchResults, setSearchResults] = useState(authUsers);
+    const [searchTerm, setSearchItem] = useState("");
+    const [isHovered, setIsHovered] = useState(false);
+    const [searchResults, setSearchResults] = useState(authUsers);
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value;
-    setSearchItem(searchTerm);
-    console.log("search term is ", searchTerm);
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm);
 
-    const filtered = authUsers.filter(
-      (user) =>
-        user.firstName?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.lastName?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.email
-          ?.substring(0, user.email?.length - 17)
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase()) ||
-        (
-          user.firstName?.toLowerCase() +
-          " " +
-          user.lastName?.toLowerCase()
-        ).includes(searchTerm.toLowerCase()),
+        if (!searchTerm.trim()) {
+            setSearchResults(authUsers);
+            return;
+        }
+
+        const filtered = authUsers.filter(
+            (user) =>
+                user.firstName?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                user.lastName?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                user.email
+                    ?.substring(0, user.email?.length - 17)
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase()) ||
+                (
+                    user.firstName?.toLowerCase() +
+                    " " +
+                    user.lastName?.toLowerCase()
+                ).includes(searchTerm.toLowerCase()),
+        );
+
+        //console.log("number of auth users", authUsers)
+
+        if (filtered?.length === 0) {
+            setSearchResults([]);
+        } else {
+            setSearchResults(filtered);
+        }
+    };
+
+    return (
+        <div className="relative w-full flex justify-center mb-4">
+            <div className="relative">
+                <Input
+                    type="search"
+                    placeholder="Search..."
+                    className="w-full md:w-[125px] lg:w-[500px] flex items-center justify-center"
+                    value={searchTerm}
+                    onChange={(e) => handleInputChange(e)}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                />
+
+                {isHovered && (
+                    <div
+                        className="absolute z-50 w-full bg-white border border-gray-200 rounded-md shadow-lg"
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                    >
+                        {searchResults.length > 0 ? (
+                            <ul className="md:space-y-4 p-4">
+                                {searchResults.slice(0,10).map((user) => (
+                                    <FriendSuggestionsBlock
+                                        suggUser={user}
+                                        curUser={curUser}
+                                        display={true}
+                                        key={user.id}
+                                    />
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>There are no authorized users.</p>
+                        )}
+                    </div>
+                )}
+            </div>
+        </div>
     );
-
-    if (filtered?.length === 0) {
-      setSearchResults([]);
-    } else {
-      setSearchResults(filtered);
-    }
-  };
-
-  return (
-    <div className="relative w-full flex justify-center mb-4">
-      <div className="relative">
-        <Input
-          type="search"
-          placeholder="Search..."
-          className="w-full md:w-[125px] lg:w-[500px] flex items-center justify-center"
-          value={searchTerm}
-          onChange={(e) => handleInputChange(e)}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        />
-
-        {isHovered && (
-          <div
-            className="absolute z-50 w-full bg-white border border-gray-200 rounded-md shadow-lg"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            {searchResults.length > 0 ? (
-              <ul className="md:space-y-4 p-4">
-                {searchResults.map((user) => (
-                  <FriendSuggestionsBlock
-                    suggUser={user}
-                    curUser={curUser}
-                    key={user.id}
-                  />
-                ))}
-              </ul>
-            ) : (
-              <p>There are no authorized users.</p>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
 }
