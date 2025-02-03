39e20923840935290d6265fb778a9804ced62e24
chouser63 <houser.ch@northeastern.edu>
2025-02-03 14:38:59 -0500

    Added popup for requests in feed

--- frontend/app/(general)/feed/page.tsx
+++ frontend/app/(general)/feed/page.tsx
@@ -4,6 +4,10 @@ import { Feed } from "@/components/feed/feed";
 import { FeedFilter } from "@/components/feed/feed-filter";
 import { FeedContainer } from "@/components/feed/feed-container";
 import { fetchAnnouncements } from "@/lib/requests/feed";
+import { getAuthorizedUserByEmail } from "@/lib/requests/authorized-user";
+import { getCurrentUser } from "@/lib/auth/session";
+import { redirect } from "next/navigation";
+import { RequestsPopupWrapper } from "@/components/friends/requests-popup-wrapper";
 
 export const metadata: Metadata = {
   title: "Feed",
@@ -17,20 +21,38 @@ export default async function FeedPage({
   searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const announcements = await fetchAnnouncements();
+
+  const user = await getCurrentUser();
+  if (!user || !user?.email) return redirect("/");
+  const authUser = await getAuthorizedUserByEmail(user.email);
+  const friendRequests = authUser.received_requests.filter(
+    (friend) =>
+      !authUser.blocked.some((blockedUser) => blockedUser.id === friend.id) &&
+      !authUser.was_blocked.some((blockedUser) => blockedUser.id === friend.id),
+  );
+
   return (
     <>
       <div className="w-full p-8 mx-auto my-4 text-center max-w-7xl">
         <h1 className="text-5xl font-bold">My Feed</h1>
       </div>
       <div className="flex flex-row justify-content ">
-      <div className="flex justify-center w-1/4 h-200  text-center">
-          <div className="w-5/6 bg-slate-100 rounded-2xl p-4">
+        <div className="flex justify-center w-1/4 h-200  text-center">
+          <div className="relative bg-slate-100 rounded-2xl p-4">
             <FriendRequests noProfile={true}></FriendRequests>
+
+            {friendRequests.length ? (
+              <div className="absolute top-[-50px] w-full">
+                <RequestsPopupWrapper user={authUser} friendships={friendRequests}></RequestsPopupWrapper>
+              </div>) : (
+              <div></div>
+            )}
+
           </div>
         </div>
-          <div className="w-3/4 h-200 text-center">
-          <FeedContainer announcements={announcements}/>
-          </div>
+        <div className="w-3/4 h-200 text-center">
+          <FeedContainer announcements={announcements} />
+        </div>
       </div>
     </>
   );
---
+++ frontend/components/friends/requests-popup-wrapper.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { RequestsPopup } from "./requests-popup";
+import { AuthorizedUser } from "@/types";
+
+export function RequestsPopupWrapper({
+  user,
+  friendships,
+}: {
+  user: AuthorizedUser;
+  friendships: AuthorizedUser[];
+}) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <div className="flex flex-row relative">
+      <Button 
+        onClick={() => setShowPopup(!showPopup)}
+        variant="link"
+        size="sm"
+        className="text-sky-300"
+      >
+        {showPopup ? 'Hide Requests' : 'Show All Requests'}
+      </Button>
+
+      {showPopup && (
+        <div className="absolute top-[50px] left-[-15px] bg-slate-100 p-3 rounded-2xl w-full">
+          <RequestsPopup user={user} friendships={friendships} />
+        </div>
+      )}
+    </div>
+  );
+}
