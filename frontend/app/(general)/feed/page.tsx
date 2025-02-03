98ce711280511a5721ef597ce617ea76796d0cc8
gpalmer27 <palmer.gi@northeastern.edu>
2025-02-03 10:33:12 -0500

    implemented filtering

--- frontend/app/(general)/feed/page.tsx
+++ frontend/app/(general)/feed/page.tsx
@@ -1,9 +1,9 @@
-
-import { defaultSort, sorting } from "@/lib/globals";
 import { Metadata } from "next";
 import { FriendRequests } from "@/components/friends/friend-requests";
 import { Feed } from "@/components/feed/feed";
 import { FeedFilter } from "@/components/feed/feed-filter";
+import { FeedContainer } from "@/components/feed/feed-container";
+import { fetchAnnouncements } from "@/lib/requests/feed";
 
 export const metadata: Metadata = {
   title: "Feed",
@@ -16,32 +16,19 @@ export default async function FeedPage({
 }: {
   searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const {
-    sort,
-    q: searchValue,
-    type,
-    focusArea,
-    tags,
-    contentType = "droplets",
-  } = (await searchParams) as { [key: string]: string };
-  const { sortKey } = sorting.find((item) => item.slug === sort) || defaultSort;
+  const announcements = await fetchAnnouncements();
   return (
     <>
       <div className="w-full p-8 mx-auto my-4 text-center max-w-7xl">
         <h1 className="text-5xl font-bold">My Feed</h1>
       </div>
       <div className="flex flex-row justify-content ">
-      <div className="w-1/4 h-200 text-center">
-        <FriendRequests />
-        </div>
-      <div className="w-1/2 h-200 text-center text-xl font-bold">
-        General Feed
-        <Feed />
-        </div>
-      <div className="w-1/4 h-200 text-center text-xl font-bold flex flex-col items-center justify-center">
-        Filters
-        <FeedFilter />
-        </div>
+        <div className="w-1/4 h-200 text-center">
+          <FriendRequests />
+          </div>
+          <div className="w-3/4 h-200 text-center">
+          <FeedContainer announcements={announcements}/>
+          </div>
       </div>
     </>
   );
