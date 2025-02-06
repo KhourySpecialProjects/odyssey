859c4596671c8917eceed51ea6e7ecf37c9bf119
gpalmer27 <palmer.gi@northeastern.edu>
2025-02-06 08:15:13 -0500

    cleaned up code

--- frontend/components/droplets/lessons/lesson-renderer.tsx
+++ frontend/components/droplets/lessons/lesson-renderer.tsx
@@ -75,15 +75,11 @@ export function LessonRenderer({
     }
 
     startTransition(async () => {
-      console.log("before success");
       const success = await markLessonAsComplete(
         enrollmentId,
         completedLessonIds,
         lesson.id,
       );
-      console.log("success status", success);
-      console.log("enrollment for mark as complete", enrollmentId);
-      console.log("lesson id for mark as complete", lesson.id);
       if (success) {
         completedLessonIds.push(lesson.id);
         await router.refresh();
