dfe09b5d22f0289e44ae0087b93acee13c3e8681
gpalmer27 <palmer.gi@northeastern.edu>
2025-02-19 12:13:26 -0500

    fixed frontend build

--- frontend/types/index.d.ts
+++ frontend/types/index.d.ts
@@ -147,7 +147,7 @@ export type Droplet = {
   isHidden: boolean;
   status: DropletStatus;
   droplet_lessons: DropletLesson[];
-  shouldBeLocked: boolean;
+  shouldBeLocked?: boolean;
 };
 
 export type QuizAnswerOption = {
