988f0cd167f93fec1a5ccad198d08d4debbaff7c
chouser63 <houser.ch@northeastern.edu>
2025-03-11 13:34:23 -0400

    visual

--- frontend/components/group/group-management-dashboard.tsx
+++ frontend/components/group/group-management-dashboard.tsx
@@ -21,6 +21,9 @@ interface RenderGroupDashboardProps {
 const tabStyle =
   "px-4 py-2 cursor-pointer border-b-2 border-transparent focus:outline-none hover:border-gray-300";
 
+  const selectedTabStyle = "dark:focus:bg-transparent dark:focus:text-white"
+
+
 export function GroupDashboard({
   group,
   canEdit,
@@ -49,7 +52,7 @@ export function GroupDashboard({
       <TabList className="flex border-b">
         <Tab className={tabStyle}>Droplets</Tab>
         <Tab className={tabStyle}>Playlists</Tab>
-        {canEdit && <Tab className={tabStyle}>Progress</Tab>}
+        {canEdit && <Tab className={tabStyle} >Progress</Tab>}
       </TabList>
       <TabPanel>
         <ContentSection
