29ab390d796132bcf8d9c9495d34546df62ea8ff
chouser63 <houser.ch@northeastern.edu>
2025-03-12 10:52:11 -0400

    friends ui changes

--- frontend/lib/requests/friends.ts
+++ frontend/lib/requests/friends.ts
@@ -566,7 +566,7 @@ export async function fetchSuggestionsById(
                 ),
             ),
           )
-          .sort((a, b) => a.lastName.localeCompare(b.lastName));
+          .sort((a, b) => a.lastName?.localeCompare(b.lastName));
       }),
     );
 
