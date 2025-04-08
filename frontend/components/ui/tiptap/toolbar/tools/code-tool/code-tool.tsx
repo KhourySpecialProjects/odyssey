75dfdf7bdb2d830cebd1bed067a4a62f40ec5f97
gpalmer27 <palmer.gi@northeastern.edu>
2025-04-08 13:22:45 -0400

    used original code

--- frontend/components/ui/tiptap/toolbar/tools/code-tool/code-tool.tsx
+++ frontend/components/ui/tiptap/toolbar/tools/code-tool/code-tool.tsx
@@ -33,7 +33,7 @@ function CodeBlockComponent({
         contentEditable={false}
         defaultValue={defaultLanguage}
         onChange={(event) => updateAttributes({ language: event.target.value })}
-        className="text-sm rounded-md px-2 py-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 dark:text-black"
+        className="text-sm rounded-md px-2 py-1 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
       >
         <option value="null">auto</option>
         <option disabled>—</option>
@@ -48,17 +48,18 @@ function CodeBlockComponent({
       <pre className="pl-12 pr-4 py-3 overflow-x-auto">
         <div className="absolute left-0 top-0 bottom-0 min-w-[2.5rem] flex flex-col text-slate-500 text-sm select-none border-r border-slate-300 bg-slate-50">
           <div className="pt-3 pl-3">
-            {Array.from({
-              length: (node.textContent.match(/\n/g) || []).length + 1,
-            }).map((_, i) => (
-              <span key={i} className="text-right pr-2 py-0.5 leading-5 block">
-                {i + 1}
-              </span>
-            ))}
+            {Array.from({ length: (node.textContent.match(/\n/g) || []).length + 1 }).map(
+              (_, i) => (
+                <span key={i} className="text-right pr-2 py-0.5 leading-5 block">
+                  {i + 1} 
+                </span>
+              )
+            )}
           </div>
         </div>
         <NodeViewContent as="code" className="block" />
       </pre>
+      
     </NodeViewWrapper>
   );
 }
@@ -68,10 +69,9 @@ function CodeTool({ editor }: { editor: Editor }) {
     <button
       onClick={() => editor.chain().focus().toggleCodeBlock().run()}
       className={cn(
-        editor?.isActive("codeBlock") ? "bg-slate-200 dark:bg-slate-700" : "",
+        editor?.isActive("codeBlock") ? "bg-slate-200" : "",
         "p-2.5 rounded-md border border-transparent hover:border-slate-200",
       )}
-      title="Code"
     >
       <CodeIcon size={17} />
     </button>
