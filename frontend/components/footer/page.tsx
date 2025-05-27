413302da7707afe91d5474e5f3a13e9170c5a821
gpalmer27 <palmer.gi@northeastern.edu>
2025-05-27 10:25:44 -0400

    moved report bug button

--- frontend/components/footer/page.tsx
+++ frontend/components/footer/page.tsx
@@ -1,24 +1,28 @@
 import Link from "next/link";
+import { ReportBugButton } from "../debug/reportBugButton";
+import { getCurrentUser } from "@/lib/auth/session";
 
-export default function Footer() {
+export default async function Footer() {
+  const user = await getCurrentUser();
   return (
-    <footer className="z-60 flex h-20 w-full grid-cols-2 items-center justify-center border-t border-t-[#919191] px-4">
-      <div className="flex scale-75 gap-x-10 md:scale-100">
-        <Link className="text-xl font-semibold" href="/about">
-          About Odyssey
-        </Link>
-        <Link className="text-xl font-semibold" href="/website-creators">
-          Website Creators
-        </Link>
-        <Link className="text-xl font-semibold" href="/content-creators">
-          Content Creators
-        </Link>
-        <div className="flex items-center">
-          <Link className="text-xl font-semibold" href="/faq">
-            FAQ
-          </Link>
-        </div>
-      </div>
-    </footer>
+    <footer className="z-60 flex h-20 w-full items-center justify-between border-t border-t-[#919191] px-4">
+    <div className="flex flex-1 items-center justify-center gap-x-10">
+      <Link className="text-xl font-semibold" href="/about">
+        About Odyssey
+      </Link>
+      <Link className="text-xl font-semibold" href="/website-creators">
+        Website Creators
+      </Link>
+      <Link className="text-xl font-semibold" href="/content-creators">
+        Content Creators
+      </Link>
+      <Link className="text-xl font-semibold" href="/faq">
+        FAQ
+      </Link>
+    </div>
+    <div className="flex items-center xs:pl-8 md:pl-0">
+      <ReportBugButton user={user} />
+    </div>
+  </footer>
   );
 }
