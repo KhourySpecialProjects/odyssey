f7adef082927001408caba5b7dd511edf3e9f37e
chouser63 <houser.ch@northeastern.edu>
2025-06-03 10:22:36 -0400

    fixed footer centering

--- frontend/components/footer/page.tsx
+++ frontend/components/footer/page.tsx
@@ -5,24 +5,29 @@ import { getCurrentUser } from "@/lib/auth/session";
 export default async function Footer() {
   const user = await getCurrentUser();
   return (
-    <footer className="z-60 flex h-20 w-full items-center justify-between border-t border-t-[#919191] px-4">
-      <div className="flex flex-1 items-center justify-center gap-x-4 md:gap-x-10">
-        <Link className="text-xl font-semibold" href="/about">
+    <footer className="z-60 flex h-20 w-full items-center border-t border-t-[#919191] px-4 gap-2">
+      <div className="flex-1"></div>
+      
+      <div className="flex items-center justify-center gap-x-4 md:gap-x-10">
+        <Link className="text-sm md:text-xl font-semibold" href="/about">
           About Odyssey
         </Link>
-        <Link className="text-xl font-semibold" href="/website-creators">
+        <Link className="text-sm md:text-xl font-semibold" href="/website-creators">
           Website Creators
         </Link>
-        <Link className="text-xl font-semibold" href="/content-creators">
+        <Link className="text-sm md:text-xl font-semibold" href="/content-creators">
           Content Creators
         </Link>
-        <Link className="text-xl font-semibold" href="/faq">
+        <Link className="text-sm md:text-xl font-semibold" href="/faq">
           FAQ
         </Link>
       </div>
-      <div className="flex items-center pl-4 md:pl-0">
+
+      <div className="flex-1 flex items-center justify-end">
         <ReportBugButton user={user} />
       </div>
+      
     </footer>
+    
   );
 }
