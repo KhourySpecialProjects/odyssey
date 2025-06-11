a1f257d177bc2c7df3e5920fd2dddf35341f8559
chouser63 <houser.ch@northeastern.edu>
2025-06-11 11:43:28 -0400

    prettier + tests

--- frontend/components/header/nav-links.tsx
+++ frontend/components/header/nav-links.tsx
@@ -17,7 +17,7 @@ export function NavLinks({
 }) {
   const pathname = usePathname();
 
-   const handleLinkClick = () => {
+  const handleLinkClick = () => {
     if (onLinkClick) {
       onLinkClick();
     }
@@ -30,18 +30,18 @@ export function NavLinks({
           key={item.href}
           className={`${pathname.startsWith(item.href.split("?")[0]) ? "font-bold" : ""}`}
         >
-            <Link
-              href={item.href}
-              onClick={handleLinkClick}
-              className={cn(
-                "block px-3 py-2",
-                pathname.startsWith(item.href.split("?")[0])
-                  ? "rounded bg-sky-700 font-bold text-white md:bg-transparent md:p-0 md:text-sky-700 md:dark:text-sky-500"
-                  : "rounded text-slate-900 hover:bg-slate-100 md:p-0 md:hover:bg-transparent md:hover:text-sky-700 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-sky-500",
-              )}
-            >
-              {item.label}
-            </Link>
+          <Link
+            href={item.href}
+            onClick={handleLinkClick}
+            className={cn(
+              "block px-3 py-2",
+              pathname.startsWith(item.href.split("?")[0])
+                ? "rounded bg-sky-700 font-bold text-white md:bg-transparent md:p-0 md:text-sky-700 md:dark:text-sky-500"
+                : "rounded text-slate-900 hover:bg-slate-100 md:p-0 md:hover:bg-transparent md:hover:text-sky-700 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-sky-500",
+            )}
+          >
+            {item.label}
+          </Link>
         </li>
       ))}
     </ul>
