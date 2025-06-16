2e617958ff3dc1a1e8fc3c41493306227b9a7983
chouser63 <houser.ch@northeastern.edu>
2025-06-16 16:44:02 -0400

    UI tweaks to fun fact box

--- frontend/components/droplets/fun-fact.tsx
+++ frontend/components/droplets/fun-fact.tsx
@@ -10,14 +10,19 @@ export function FunFact({
 }) {
 
     return (
-        <div className="flex flex-col gap-2 items-center bg-slate-50 border border-slate-200 p-6 mt-12 rounded-lg">
-            <p className="text-lg text-left text-slate-600 dark:text-slate-300"><strong className="text-2xl text-black mr-2">Did you know?</strong>{droplet.funFact}</p>
+        <div className="flex flex-col gap-2 items-center bg-slate-50 border border-slate-200 dark:bg-slate-800 dark:border-slate-500 p-6 mt-12 rounded-lg">
+            <p className="text-lg text-left text-slate-600 dark:text-slate-300"><strong className="text-2xl text-black mr-2 dark:text-slate-300">Did you know?</strong>{droplet.funFact}</p>
             <Link
                 className="w-full"
                 href={`/d/${droplet.slug}`}
             >
-                <Button className="w-1/2 text-lg bg-[#8BB5E7] text-slate-900 border border-slate-400"
-                    after={<ArrowRightIcon />}>Dive deeper in <strong>{droplet.name}</strong></Button>
+                <Button 
+                    className="w-1/2 h-auto text-lg bg-sky-200 hover:scale-105 hover:bg-sky-200 dark:bg-sky-200 dark:hover:bg-sky-200 text-slate-900"
+                >
+                    <p className="whitespace-normal text-wrap">Dive deeper in <strong className="">{droplet.name}</strong></p>
+                    <ArrowRightIcon />
+                </Button>
+                
             </Link>
         </div>
     )
