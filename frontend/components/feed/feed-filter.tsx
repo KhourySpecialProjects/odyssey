d1ac4f0e333b8cecaf58f5da7647ac6011fb2f8a
chouser63 <houser.ch@northeastern.edu>
2025-06-24 11:42:26 -0400

    fixed filter formatting for better spacing

--- frontend/components/feed/feed-filter.tsx
+++ frontend/components/feed/feed-filter.tsx
@@ -29,59 +29,39 @@ export function FeedFilter({ onFilterChange }: FeedFilterProps) {
   const roleOptions = [
     {
       value: AnnouncementTypeTitle.Droplet,
-      label: (
-        <>
-          <span role="droplet">Droplet</span>
-          <Droplet size={20} />
-        </>
-      ),
-      color: "bg-blue-200 dark:bg-[#266697] dark:text-slate-300",
+      label: "Droplet",
+      icon: <Droplet size={20} />,
+      color: "bg-blue-200 dark:bg-[#266697] dark:text-slate-200",
     },
     {
       value: AnnouncementTypeTitle.Playlist,
-      label: (
-        <>
-          <span role="playlist">Playlist</span>
-          <ListVideo size={20} />
-        </>
-      ),
-      color: "bg-green-200 dark:bg-[#29703B] dark:text-slate-300",
+      label: "Playlist",
+      icon: <ListVideo size={20} />,
+      color: "bg-green-200 dark:bg-[#29703B] dark:text-slate-200",
     },
     {
       value: AnnouncementTypeTitle.Group,
-      label: (
-        <>
-          <span role="group">Group</span> <UsersRound size={20} />
-        </>
-      ),
-      color: "bg-purple-200 dark:bg-[#754ABA] dark:text-slate-300",
+      label: "Group",
+      icon: <UsersRound size={20} />,
+      color: "bg-purple-200 dark:bg-[#754ABA] dark:text-slate-200",
     },
     {
       value: AnnouncementTypeTitle.System,
-      label: (
-        <>
-          <span role="system">System</span> <Info size={20} />
-        </>
-      ),
-      color: "bg-red-200 dark:bg-[#B83028] dark:text-slate-300",
+      label: "System",
+      icon: <Info size={20} />,
+      color: "bg-red-200 dark:bg-[#B83028] dark:text-slate-200",
     },
     {
       value: AnnouncementTypeTitle.Friend,
-      label: (
-        <>
-          <span role="friend">Friend</span> <Handshake size={20} />
-        </>
-      ),
-      color: "bg-yellow-200 dark:bg-[#977020] dark:text-slate-300",
+      label: "Friend",
+      icon: <Handshake size={20} />,
+      color: "bg-yellow-200 dark:bg-[#C38508] dark:text-slate-200",
     },
     {
       value: AnnouncementTypeTitle.Kudos,
-      label: (
-        <>
-          <span role="kudos">Kudos</span> <PartyPopper size={20} />
-        </>
-      ),
-      color: "bg-orange-200 dark:bg-[#B55E0C] dark:text-slate-300",
+      label: "Kudos",
+      icon: <PartyPopper size={20} />,
+      color: "bg-orange-200 dark:bg-[#B55E0C] dark:text-slate-200",
     },
   ] as const;
 
@@ -99,7 +79,33 @@ export function FeedFilter({ onFilterChange }: FeedFilterProps) {
   };
 
   return (
-    <div className="gap-2 gap-x-0 py-4 sm:grid sm:grid-cols-1 md:grid-cols-2 md:rounded-md md:border md:border-slate-200 md:bg-slate-50 md:dark:border-slate-500 md:dark:bg-slate-800">
+    <div className="p-4 grid sm:grid-cols-1 md:grid-cols-2 gap-3 md:rounded-md md:border md:border-slate-200 md:bg-slate-50 md:dark:border-slate-500 md:dark:bg-slate-800">
+      {roleOptions.map((role) => (
+        <div
+          key={role.value}
+          className={`flex justify-between items-center rounded-md p-2 ${role.color}`}
+        >
+          <Checkbox
+            id={role.value}
+            checked={selectedRoles.includes(role.value)}
+            onCheckedChange={() => toggleRole(role.value)}
+            className="border-sky-500 bg-sky-200 focus-visible:ring-sky-500 data-[state=checked]:border-sky-500 data-[state=checked]:bg-sky-500 dark:data-[state=checked]:bg-sky-500"
+          />
+          <span 
+            className={`text-sm font-medium ${selectedRoles.includes(role.value) ? "opacity-100" : "opacity-50"}`}
+          >
+            {role.label}
+          </span>
+          <div className={`${selectedRoles.includes(role.value) ? "opacity-100" : "opacity-50"}`}>
+            {role.icon}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+{/*<div className="gap-2 gap-x-0 py-4 sm:grid sm:grid-cols-1 md:grid-cols-2 md:rounded-md md:border md:border-slate-200 md:bg-slate-50 md:dark:border-slate-500 md:dark:bg-slate-800">
       {roleOptions.map((role) => (
         <div
           key={role.value}
@@ -120,6 +126,4 @@ export function FeedFilter({ onFilterChange }: FeedFilterProps) {
           </div>
         </div>
       ))}
-    </div>
-  );
-}
+    </div>*/}
