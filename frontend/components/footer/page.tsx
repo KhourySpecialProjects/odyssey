f1e4ef834130be68fd63c642dfbd87b8ac050cb7
chouser63 <houser.ch@northeastern.edu>
2025-07-02 12:05:16 -0500

    prettier

--- frontend/components/footer/page.tsx
+++ frontend/components/footer/page.tsx
@@ -1,47 +1,44 @@
 import Link from "next/link";
 import { ReportBugButton } from "../debug/reportBugButton";
 import { getCurrentUser } from "@/lib/auth/session";
-import Wave from 'react-wavify';
+import Wave from "react-wavify";
 import { Separator } from "../ui/separator";
 import Image from "next/image";
 import { DarkMode } from "../explore/dark-mode";
 
 export default async function Footer() {
   const user = await getCurrentUser();
-  const linkStyles = "text-sm font-semibold md:text-xl text-white hover:scale-105";
+  const linkStyles =
+    "text-sm font-semibold md:text-xl text-white hover:scale-105";
   return (
-    <footer className="z-60 flex flex-col w-full items-center bg-[#83C1E1] dark:bg-[#3A6B85]">
-      <Wave fill='#2F5569'
+    <footer className="z-60 flex w-full flex-col items-center bg-[#83C1E1] dark:bg-[#3A6B85]">
+      <Wave
+        fill="#2F5569"
         paused={false}
-        style={{ display: 'flex',
-         }}
+        style={{ display: "flex" }}
         options={{
           height: 2,
           amplitude: 20,
           speed: 0.15,
-          points: 3
+          points: 3,
         }}
         className="bg-transparent dark:bg-transparent"
       />
 
-
-      <div className="w-full min-h-[250px] bg-[#2F5569] flex flex-row justify-between px-6 md:px-16 pt-4 pb-24 -my-10">
-        <div className="flex flex-row justify-start gap-4 w-1/2 md:w-1/4">
-          <Separator orientation={"vertical"} className="bg-slate-200 dark:bg-slate-200 h-full w-[3px]" />
+      <div className="-my-10 flex min-h-[250px] w-full flex-row justify-between bg-[#2F5569] px-6 pt-4 pb-24 md:px-16">
+        <div className="flex w-1/2 flex-row justify-start gap-4 md:w-1/4">
+          <Separator
+            orientation={"vertical"}
+            className="h-full w-[3px] bg-slate-200 dark:bg-slate-200"
+          />
           <div className="flex flex-col items-center justify-center gap-10 py-4">
             <Link className={linkStyles} href="/about">
               About Odyssey
             </Link>
-            <Link
-              className={linkStyles}
-              href="/website-creators"
-            >
+            <Link className={linkStyles} href="/website-creators">
               Website Creators
             </Link>
-            <Link
-              className={linkStyles}
-              href="/content-creators"
-            >
+            <Link className={linkStyles} href="/content-creators">
               Content Creators
             </Link>
             <Link className={linkStyles} href="/faq">
@@ -49,10 +46,10 @@ export default async function Footer() {
             </Link>
           </div>
         </div>
-        <div className="hidden md:flex items-center justify-center w-1/2">
+        <div className="hidden w-1/2 items-center justify-center md:flex">
           <Link href="/">
             <Image
-              src={'/circular_logo.svg'}
+              src={"/circular_logo.svg"}
               alt="Khoury Odyssey Logo"
               width={150}
               height={150}
@@ -60,11 +57,15 @@ export default async function Footer() {
             />
           </Link>
         </div>
-        <div className="flex flex-col justify-between items-end w-1/2 md:w-1/4">
-          <Link href="https://github.com/KhourySpecialProjects/odyssey" legacyBehavior role="link">
+        <div className="flex w-1/2 flex-col items-end justify-between md:w-1/4">
+          <Link
+            href="https://github.com/KhourySpecialProjects/odyssey"
+            legacyBehavior
+            role="link"
+          >
             <a target="_blank" rel="noopener noreferrer">
               <Image
-                src={'/github.svg'}
+                src={"/github.svg"}
                 alt="Odyssey Github Repo"
                 width={50}
                 height={50}
@@ -73,12 +74,10 @@ export default async function Footer() {
               />
             </a>
           </Link>
-          <DarkMode className="scale-[1.2] mx-2" />
+          <DarkMode className="mx-2 scale-[1.2]" />
           <ReportBugButton user={user} />
         </div>
       </div>
-
-
     </footer>
   );
 }
