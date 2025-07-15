c8da487bd69848e088bcaab1c8e03740a68bb0f8
chouser63 <houser.ch@northeastern.edu>
2025-07-15 10:31:24 -0500

    added slideshow component to staggered gallery and formatted

--- frontend/components/ui/staggered-gallery.tsx
+++ frontend/components/ui/staggered-gallery.tsx
@@ -1,5 +1,7 @@
 import { Slideshow } from "./slideshow";
 
+
+
 function StaggeredGallery({
   title,
   descriptions,
@@ -9,29 +11,49 @@ function StaggeredGallery({
   descriptions: string[];
   images: string[][];
 }) {
-  /*
-    Admin: you can edit both the description and images
-
-    User: all you can do is view
-    */
 
   return (
-    <div className="mx-auto h-screen w-full border border-slate-400 text-center">
-      <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl dark:text-white">
-        {title}
-      </h1>
+    <div className="mx-auto h-full w-full text-center pt-16 pb-8 space-y-8">
+      <div className="mx-auto max-w-2xl text-center">
+          <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl dark:text-white">
+            {title}
+          </h1>
+          <p className="mt-4 text-lg leading-normal text-balance text-slate-600 dark:text-slate-300">
+            Check out Odyssey's many features!
+          </p>
+        </div>
 
-      <Slideshow images={images[0]} />
 
-      {/*<ul className="p-4 md:space-y-4">
-                {images.map((image, index) => {
-                    return (<img
-                        src={imagePreview}
-                        alt="Profile"
-                        className="mb-4 h-32 w-32 rounded-full border object-cover"
-                    />)
-                })}
-            </ul>*/}
+      <ul className="p-4 md:space-y-8">
+        {images.map((image, index) => {
+          return (
+            <div key={`row-${index}`}>
+              {index % 2 == 0 ? (
+                <div className="flex flex-row justify-center space-x-8 px-8">
+                  <div className="">
+                    <Slideshow images={image} size={500} />
+                  </div>
+                  <div className="text-left space-y-2">
+                    <h1 className="text-3xl font-bold">Title</h1>
+                    <p className="text-2xl leading-relaxed">{descriptions[index]}</p>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex flex-row justify-center space-x-8 px-8">
+                  <div className="text-left space-y-2">
+                    <h1 className="text-3xl font-bold">Title</h1>
+                    <p className="text-2xl leading-relaxed">{descriptions[index]}</p>
+                  </div>
+                  <div className="">
+                    <Slideshow images={image} size={500} />
+                  </div>
+                </div>
+              )
+              }
+            </div>
+          )
+        })}
+      </ul>
     </div>
   );
 }
