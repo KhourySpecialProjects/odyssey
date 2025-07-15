3ef47e686d33fd6502ed96c8077ce335fc1e342f
chouser63 <houser.ch@northeastern.edu>
2025-07-14 21:40:19 -0500

    created staggered gallery and slideshow components for features page

---
+++ frontend/components/ui/staggered-gallery.tsx
@@ -0,0 +1,47 @@
+import { Slideshow } from "./slideshow";
+
+
+
+
+function StaggeredGallery({
+    title,
+    descriptions,
+    images,
+}: {
+    title: string;
+    descriptions: string[];
+    images: string[][];
+}) {
+
+    /*
+    Admin: you can edit both the description and images
+
+    User: all you can do is view
+    */
+
+
+    return (
+        <div className="w-full h-screen border border-slate-400 mx-auto text-center">
+            <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl dark:text-white">
+                {title}
+            </h1>
+
+            <Slideshow images={images[0]}/>
+
+            {/*<ul className="p-4 md:space-y-4">
+                {images.map((image, index) => {
+                    return (<img
+                        src={imagePreview}
+                        alt="Profile"
+                        className="mb-4 h-32 w-32 rounded-full border object-cover"
+                    />)
+                })}
+            </ul>*/}
+        </div>
+
+    )
+
+
+}
+
+export { StaggeredGallery }
