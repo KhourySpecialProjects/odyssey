9a9bdfe7d5f7ebece7a46b1a6b380d9bca907464
chouser63 <houser.ch@northeastern.edu>
2025-07-14 21:41:00 -0500

    prettier

--- frontend/components/ui/slideshow.tsx
+++ frontend/components/ui/slideshow.tsx
@@ -1,54 +1,44 @@
-
-'use client'
+"use client";
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
 
+function Slideshow({ images }: { images: string[] }) {
+  const [slideshowPosition, setSlideshowPosition] = useState(0);
 
-function Slideshow({
-    images,
-}: {
-    images: string[];
-}) {
+  return (
+    <div className="relative mx-auto flex h-[300px] w-[300px] justify-between border border-slate-200 bg-slate-50 text-center">
+      <button
+        onClick={() => setSlideshowPosition((prev) => prev - 1)}
+        className={`z-50`}
+        disabled={slideshowPosition <= 0}
+      >
+        <ChevronLeft size={64} color="#504e4e" />
+      </button>
 
-    const [slideshowPosition, setSlideshowPosition] = useState(0);
+      <ul className="">
+        {images.map((image, index) => {
+          return (
+            <div key={`${index}`} className="z-10">
+              <img
+                src={image}
+                alt="Profile"
+                className={`transition-scale absolute z-10 h-full w-full ${index === slideshowPosition ? "right-0" : "right-full opacity-0"} text-gray-900 duration-500 ease-in-out`}
+              />
+            </div>
+          );
+        })}
+      </ul>
 
-
-
-
-
-    return (
-        <div className="bg-slate-50 border border-slate-200 mx-auto text-center relative flex justify-between h-[300px] w-[300px]">
-            <button onClick={() => setSlideshowPosition(prev => prev - 1)}
-                className={`z-50`}
-                disabled={slideshowPosition <= 0}>
-                <ChevronLeft size={64} color="#504e4e" />
-            </button>
-
-            <ul className="">
-                {images.map((image, index) => {
-                    return (
-                        <div key={`${index}`} className="z-10">
-                            <img
-                                src={image}
-                                alt="Profile"
-                                className={`z-10 h-full w-full transition-scale absolute ${index === slideshowPosition ? 'right-0' : 'right-full opacity-0'} text-gray-900 duration-500 ease-in-out`}
-                            />
-                        </div>)
-                })}
-            </ul>
-
-            <button onClick={() => setSlideshowPosition(prev => prev + 1)}
-                className={`z-50`}
-                disabled={slideshowPosition >= images.length - 1}>
-                <ChevronRight size={64} color="#504e4e" />
-            </button>
-
-        </div>
-
-    )
-
-
+      <button
+        onClick={() => setSlideshowPosition((prev) => prev + 1)}
+        className={`z-50`}
+        disabled={slideshowPosition >= images.length - 1}
+      >
+        <ChevronRight size={64} color="#504e4e" />
+      </button>
+    </div>
+  );
 }
 
-export { Slideshow }
+export { Slideshow };
