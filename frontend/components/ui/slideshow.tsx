f9b108853af452db14a14f6c014c5faf5edfd674
chouser63 <houser.ch@northeastern.edu>
2025-07-14 21:49:00 -0500

    hid slideshow overflow

--- frontend/components/ui/slideshow.tsx
+++ frontend/components/ui/slideshow.tsx
@@ -7,13 +7,13 @@ function Slideshow({ images }: { images: string[] }) {
   const [slideshowPosition, setSlideshowPosition] = useState(0);
 
   return (
-    <div className="relative mx-auto flex h-[300px] w-[300px] justify-between border border-slate-200 bg-slate-50 text-center">
+    <div className="relative mx-auto flex h-[300px] w-[300px] justify-between border border-slate-200 bg-slate-50 text-center overflow-hidden">
       <button
         onClick={() => setSlideshowPosition((prev) => prev - 1)}
         className={`z-50`}
         disabled={slideshowPosition <= 0}
       >
-        <ChevronLeft size={64} color="#504e4e" />
+        <ChevronLeft size={64} color="#8d8b8bff" />
       </button>
 
       <ul className="">
@@ -35,7 +35,7 @@ function Slideshow({ images }: { images: string[] }) {
         className={`z-50`}
         disabled={slideshowPosition >= images.length - 1}
       >
-        <ChevronRight size={64} color="#504e4e" />
+        <ChevronRight size={64} color="#8d8b8bff" />
       </button>
     </div>
   );
