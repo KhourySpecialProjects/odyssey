70c5e2a4779608899454bca8ec60d93145e8ed80
chouser63 <houser.ch@northeastern.edu>
2025-08-11 10:26:00 -0500

    changed droplet tile layout to align rating to bottom

--- frontend/components/droplets/droplet-tile.tsx
+++ frontend/components/droplets/droplet-tile.tsx
@@ -152,99 +152,101 @@ export function DropletTile({
             </span>
           </div>
         </Button>
-        <div className="flex flex-col justify-end gap-3 p-4">
-          <div className="flex flex-0 flex-row flex-wrap gap-1.5">
-            {droplet.status == "draft" ? (
-              <Badge variant="destructive">Draft</Badge>
-            ) : null}
+        <div className="flex flex-col justify-between gap-3 p-4 h-full">
+          <div className="space-y-3">
+            <div className="flex flex-0 flex-row flex-wrap gap-1.5">
+              {droplet.status == "draft" ? (
+                <Badge variant="destructive">Draft</Badge>
+              ) : null}
 
-            {dueDate && dueDate !== "" && daysUntil > -2 && (
-              <Badge
-                className={getDueDateBadgeColor(daysUntil, true)}
-                variant="outline"
-              >
-                <Clock size={15} className="mr-1" />
+              {dueDate && dueDate !== "" && daysUntil > -2 && (
+                <Badge
+                  className={getDueDateBadgeColor(daysUntil, true)}
+                  variant="outline"
+                >
+                  <Clock size={15} className="mr-1" />
 
-                {(() => {
-                  if (
-                    DateTime.fromISO(dueDate).toISODate() ==
-                    DateTime.local().toISODate()
-                  ) {
-                    return "Due today!";
-                  } else if (daysUntil === 1) {
-                    return `Due in 1 day`;
-                  } else if (daysUntil > 0) {
-                    return `Due in ${daysUntil} days`;
-                  } else {
-                    return "Late!";
-                  }
-                })()}
-              </Badge>
-            )}
-
-            {isEnrolled && dropletLessonIds.length > 0 && (
-              <Badge className={getCompletionBadgeColor()} variant="outline">
-                {completionPercentage}% Complete
-              </Badge>
-            )}
-
-            <Badge className="pointer-events-none border-black bg-white text-black dark:bg-slate-300">
-              {uppercaseFirstChar(droplet.focusArea)}
-            </Badge>
-            <Badge className="pointer-events-none border-black bg-white text-black dark:bg-slate-300">
-              {uppercaseFirstChar(droplet.type)}
-            </Badge>
-            {droplet.tags?.map((tag) => (
-              <Badge
-                key={tag.id}
-                className="pointer-events-none border-black bg-white text-black dark:bg-slate-300"
-              >
-                {tag.name}
-              </Badge>
-            ))}
-          </div>
-          <div className="flex flex-col justify-center gap-1">
-            <span className="block w-full place-self-end text-3xl font-black text-slate-950 dark:text-slate-300">
-              {droplet.name}
-            </span>
-
-            {droplet.description &&
-              droplet.description.trim() !== "<p></p>" &&
-              droplet.description.trim() !== "" && (
-                <>
-                  <p
-                    className={`${descriptionExpanded ? "line-clamp-none" : "line-clamp-2"
-                      } text-md font-black text-slate-700 dark:text-slate-300`}
-                  >
-                    {droplet.description}
-                  </p>
-                  <p>
-                    {descriptionExpanded ? (
-                      <button
-                        onClick={(e) => {
-                          e.preventDefault();
-                          setDescriptionExpanded(false);
-                        }}
-                        className="text-sm text-sky-700 dark:text-slate-300"
-                      >
-                        See Less
-                      </button>
-                    ) : (
-                      <button
-                        onClick={(e) => {
-                          e.preventDefault();
-                          setDescriptionExpanded(true);
-                        }}
-                        className="text-sm text-sky-700 dark:text-slate-300"
-                      >
-                        See More
-                      </button>
-                    )}
-                  </p>
-                </>
+                  {(() => {
+                    if (
+                      DateTime.fromISO(dueDate).toISODate() ==
+                      DateTime.local().toISODate()
+                    ) {
+                      return "Due today!";
+                    } else if (daysUntil === 1) {
+                      return `Due in 1 day`;
+                    } else if (daysUntil > 0) {
+                      return `Due in ${daysUntil} days`;
+                    } else {
+                      return "Late!";
+                    }
+                  })()}
+                </Badge>
               )}
 
+              {isEnrolled && dropletLessonIds.length > 0 && (
+                <Badge className={getCompletionBadgeColor()} variant="outline">
+                  {completionPercentage}% Complete
+                </Badge>
+              )}
 
+              <Badge className="pointer-events-none border-black bg-white text-black dark:bg-slate-300">
+                {uppercaseFirstChar(droplet.focusArea)}
+              </Badge>
+              <Badge className="pointer-events-none border-black bg-white text-black dark:bg-slate-300">
+                {uppercaseFirstChar(droplet.type)}
+              </Badge>
+              {droplet.tags?.map((tag) => (
+                <Badge
+                  key={tag.id}
+                  className="pointer-events-none border-black bg-white text-black dark:bg-slate-300"
+                >
+                  {tag.name}
+                </Badge>
+              ))}
+            </div>
+            <div className="flex flex-col justify-center gap-1">
+              <span className="block w-full place-self-end text-3xl font-black text-slate-950 dark:text-slate-300">
+                {droplet.name}
+              </span>
+
+              {droplet.description &&
+                droplet.description.trim() !== "<p></p>" &&
+                droplet.description.trim() !== "" && (
+                  <>
+                    <p
+                      className={`${descriptionExpanded ? "line-clamp-none" : "line-clamp-2"
+                        } text-md font-black text-slate-700 dark:text-slate-300`}
+                    >
+                      {droplet.description}
+                    </p>
+                    <p>
+                      {descriptionExpanded ? (
+                        <button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setDescriptionExpanded(false);
+                          }}
+                          className="text-sm text-sky-700 dark:text-slate-300"
+                        >
+                          See Less
+                        </button>
+                      ) : (
+                        <button
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setDescriptionExpanded(true);
+                          }}
+                          className="text-sm text-sky-700 dark:text-slate-300"
+                        >
+                          See More
+                        </button>
+                      )}
+                    </p>
+                  </>
+                )}
+
+
+            </div>
           </div>
 
           {averageRating != 0 ? (
