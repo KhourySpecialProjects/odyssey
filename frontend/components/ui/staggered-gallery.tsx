cf75016bf62efbb6dced5b8a43fdb6ef4967f297
chouser63 <houser.ch@northeastern.edu>
2025-08-13 11:21:48 -0500

    improved features page spacing and font colors

--- frontend/components/ui/staggered-gallery.tsx
+++ frontend/components/ui/staggered-gallery.tsx
@@ -3,9 +3,9 @@ import { Slideshow } from "./slideshow";
 
 function StaggeredGallery({ gallery }: { gallery: Gallery }) {
   return (
-    <div className="mx-auto h-full w-full space-y-8 pb-8 text-center">
+    <div className="mx-auto h-full w-full space-y-12 pb-8 text-center">
       <div className="mx-auto max-w-2xl text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl dark:text-white">
+        <h1 className="text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl dark:text-white">
           {gallery.title}
         </h1>
         <p className="mt-4 text-lg leading-normal text-balance text-slate-600 dark:text-slate-300">
@@ -13,19 +13,19 @@ function StaggeredGallery({ gallery }: { gallery: Gallery }) {
         </p>
       </div>
 
-      <ul className="space-y-8 p-4">
+      <ul className="space-y-20 p-4">
         {gallery.items.map((galleryItem, index) => {
           const isEven = index % 2 === 0;
           return (
             <li key={`row-${index}`}>
-              <div className="flex flex-col justify-center md:gap-8 md:px-8 lg:flex-row">
+              <div className="flex flex-col justify-center items-center md:gap-8 md:px-8 lg:flex-row">
                 <div
                   className={`order-1 ${isEven ? "lg:order-1" : "lg:order-2"} mb-4 space-y-2 text-left lg:mb-0 lg:w-1/2`}
                 >
                   <h1 className="text-2xl font-bold sm:text-3xl">
                     {galleryItem.title || "Title"}
                   </h1>
-                  <p className="text-xl leading-relaxed sm:text-2xl">
+                  <p className="text-xl leading-relaxed sm:text-2xl text-slate-700 dark:text-white">
                     {galleryItem.description}
                   </p>
                 </div>
