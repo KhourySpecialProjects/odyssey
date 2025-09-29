f1791da5bd5ff2c6eb0a76d723719bc4493ceac0
Wesley Chapman <w.chapman@northeastern.edu>
2025-09-29 13:52:10 -0400

    implemented retention rate statistic on admin page

---
+++ frontend/lib/requests/analytics.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { fetchDroplets } from "./data";
+import { fetchEnrollmentMetadata } from "./enrollment";
+import { fetchAPI } from "../utils";
+import { Droplet, Enrollment } from "@/types";
+import { StrapiRequestParams } from "@/types/strapi";
+
+// Function to get all enrollments
+async function getCompletedEnrollmentsCount(): Promise<number> {
+  const response = await fetchEnrollmentMetadata({
+    filters: { isComplete: { $eq: true } },
+    pagination: { pageSize: 1, page: 1 },
+  });
+
+  return response?.meta?.pagination?.total || 0;
+}
+
+export async function getRetentionData() {
+  const [enrollmentMetadata, completedCount] = await Promise.all([
+    fetchEnrollmentMetadata(),
+    getCompletedEnrollmentsCount(),
+  ]);
+
+  const totalEnrollments = enrollmentMetadata?.meta?.pagination?.total || 0;
+  const completedEnrollments = completedCount;
+
+  console.log("Debug - Total enrollments:", totalEnrollments);
+  console.log("Debug - Completed enrollments:", completedEnrollments);
+
+  const retentionRate =
+    totalEnrollments === 0
+      ? 0
+      : Math.round((completedEnrollments / totalEnrollments) * 10000) / 100;
+
+  return {
+    retentionRate,
+    totalEnrollments,
+    completedEnrollments,
+  };
+}
