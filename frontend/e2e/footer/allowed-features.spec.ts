8189ed648065f7d5f6146b7385e6b7f12b7f6e9f
DSCoder555 <j.almanzar@northeastern.edu>
2025-10-27 11:18:00 -0400

    ODY-130 capitalized title

--- frontend/e2e/footer/allowed-features.spec.ts
+++ frontend/e2e/footer/allowed-features.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe("Allowed footer navigation tests", () => {
+test.describe("Allowed Footer Navigation Tests", () => {
   test('Navigate To Landing Page', async ({ page }) => {
     await page.goto('http://localhost:3000/');
     await page.getByRole('link', { name: 'About Odyssey' }).click();
