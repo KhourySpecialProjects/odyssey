4338a4acb14289843a166efa828c327017354682
DSCoder555 <j.almanzar@northeastern.edu>
2025-10-27 11:23:41 -0400

    ODY-130 capitalized github

--- frontend/e2e/footer/allowed-features.spec.ts
+++ frontend/e2e/footer/allowed-features.spec.ts
@@ -33,7 +33,7 @@ test.describe("Allowed Footer Navigation Tests", () => {
     await expect(page.getByRole('heading', { name: 'Website Creators' })).toBeVisible();
     await expect(page.getByRole('heading', { name: 'Content Creators' })).toBeVisible();
   });
-  test('Go to github', async ({ page }) => {
+  test('Go to Github', async ({ page }) => {
     await page.goto('http://localhost:3000/');
     await expect(page.getByRole('link', { name: 'Khoury Odyssey Logo Odyssey,' })).toBeVisible();
 
