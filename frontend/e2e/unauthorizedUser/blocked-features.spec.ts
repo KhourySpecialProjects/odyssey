da5aae43b031935be24cdc759d74ccab47ace81a
DSCoder555 <j.almanzar@northeastern.edu>
2025-10-29 12:04:39 -0400

    ODY-131 prettier

--- frontend/e2e/unauthorizedUser/blocked-features.spec.ts
+++ frontend/e2e/unauthorizedUser/blocked-features.spec.ts
@@ -1,13 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Unauthorized Navigation Tests", () => {
-    test.use({
-        storageState: "e2e/unauthorizedUser/auth.json",
-    });
+  test.use({
+    storageState: "e2e/unauthorizedUser/auth.json",
+  });
 
-    test("Navigate to Dashboard", async ({page}) => {
-
-    })
+  test("Navigate to Dashboard", async ({ page }) => {});
 });
 
 test.describe("Unauthorized Workflow Tests", () => {});
