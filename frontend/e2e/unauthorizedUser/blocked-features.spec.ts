0459c384b49c2808a5858f2596f931a3017a18a3
DSCoder555 <j.almanzar@northeastern.edu>
2025-10-29 11:55:47 -0400

    ODY-131 updated more scripts for role-specific tests to be ran

--- frontend/e2e/unauthorizedUser/blocked-features.spec.ts
+++ frontend/e2e/unauthorizedUser/blocked-features.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Unauthorized Navigation Tests", () => {});
+test.describe("Unauthorized Navigation Tests", () => {
+    test.use({
+        storageState: "e2e/unauthorizedUser/auth.json",
+    });
+
+    test("Navigate to Dashboard", async ({page}) => {
+
+    })
+});
 
 test.describe("Unauthorized Workflow Tests", () => {});
