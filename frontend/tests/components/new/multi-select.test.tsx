4c1aafee504bbfeafd67aac2e4c51acc8b8180ff
DSCoder555 <j.almanzar@northeastern.edu>
2025-11-20 16:30:45 -0500

    ODY-221 update test of input box to Select Similar Droplets...

--- frontend/tests/components/new/multi-select.test.tsx
+++ frontend/tests/components/new/multi-select.test.tsx
@@ -61,7 +61,7 @@ describe("MultiSelect", () => {
           setSelected={mockSetSelected}
         />,
       );
-      expect(screen.getByText("Select Postrequisites...")).toBeInTheDocument();
+      expect(screen.getByText("Select Similar Droplets...")).toBeInTheDocument();
     });
 
     it("shows selected items as badges", () => {
