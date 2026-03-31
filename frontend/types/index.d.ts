68a990b7ce5cab34d78aaf9cefd6818e79282d49
Ricardo Landeros <r.landeros15@yahoo.com>
2026-03-31 16:42:18 -0400

    feat(types): add slideLayout properties to generic block type

--- frontend/types/index.d.ts
+++ frontend/types/index.d.ts
@@ -139,6 +139,8 @@ export type Block =
       id?: number;
       sourceBlockIds?: number[]; // For grouped blocks - all source BlockNote block IDs
       _clientId?: string;
+      slideLayout?: "image-left" | "image-right" | "full-image";
+      slideLayoutImageUrl?: string;
     }
   | {
       __component: "droplets.expandable";
