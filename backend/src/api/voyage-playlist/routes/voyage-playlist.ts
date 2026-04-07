67eabae104b2915dd1059ec36dcbdaaa21dcb3d3
Ricardo Landeros <r.landeros15@yahoo.com>
2026-04-07 14:08:13 -0400

    Revert "fix: remove voyages backend (missing generated types break build)"
    This reverts commit 87b64e89ffeff17a2eeb01f970f77f116bb7d7f9.

---
+++ backend/src/api/voyage-playlist/routes/voyage-playlist.ts
@@ -0,0 +1,7 @@
+/**
+ * voyage-playlist router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::voyage-playlist.voyage-playlist');
---
+++ backend/src/api/voyage-playlist/services/voyage-playlist.ts
@@ -0,0 +1,7 @@
+/**
+ * voyage-playlist service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::voyage-playlist.voyage-playlist');
